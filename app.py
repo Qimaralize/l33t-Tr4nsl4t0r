2620c0b4273f9f82069924ae08894bdd94b856fe
Qimaralize <core0of0devil@gmail.com>
2025-04-23 08:24:04 +1000

    add reverse translate

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ root = Tk()
 root.title('l33t tr4nsl4t0r')
 root.geometry('250x250')
 
+#Добавление названия
+label_of_entry = ttk.Label()
+label_of_entry['text'] = 'Enter text that you need to translate to l33t'
+label_of_entry.pack(anchor=CENTER, padx = 10, pady = 20)
+
+
 #Добавление поля ввода
 entry = ttk.Entry()
 entry.pack(anchor=CENTER, padx = 10, pady = 10)
@@ -48,7 +54,7 @@ def to_leet_speak(str):
 #функция для получения текта
 def get_translation_of_text():
     global entered_text
-    entered_text = entry.get()
+    entered_text = entry.get().upper()
     label['text'] = to_leet_speak(entered_text)
     
 
@@ -60,6 +66,65 @@ label.pack(anchor=CENTER, padx=10, pady=30)
 #кнопка для перевода 
 btn = ttk.Button(text='translate', command=get_translation_of_text)
 btn.pack(anchor=E, padx=5, pady=10)
+
+#Добавление альтернативного перевода
+label_of_alt_translate = Label()
+label_of_alt_translate['text'] = 'Enter text you need to translate from l33t'
+label_of_alt_translate.pack(anchor=S, pady = 40)
+
+alt_entry = ttk.Entry()
+alt_entry.pack(anchor=S, pady = 60)
+
+#Функция для альт перевода
+
+def from_leet_speak(alt_str):
+    alt_leet = {
+  '@' : 'A',
+  '8' : 'B',
+  '(' : 'C',
+  'D' : 'D',
+  '3' : 'E',
+  'F' : 'F',
+  '6' : 'G',
+  '#': 'H',
+  '!' : 'I',
+  'J' : 'J',
+  'K' : 'K',
+  '1' : 'L',
+  'M' : 'M',
+  'N' : 'N',
+  '0' : 'O',
+  'P' : 'P',
+  'Q' : 'Q',
+  'R' : 'R',
+  '$' : 'S',
+  '7' : 'T',
+  'U' : 'U',
+  'V' : 'V',
+  'W' : 'W',
+  'X' : 'X',
+  'Y' : 'Y',
+  '2' : 'Z'
+}
+    for key in alt_leet:
+        alt_str = alt_str.replace(key, alt_leet[key])
+    return alt_str
+
+#Функция для отображения альт перевода
+def get_alt_translation_of_text():
+    global entered_alt_text
+    entered_alt_text = alt_entry.get()
+    alt_trans_lbl['text'] = from_leet_speak(entered_alt_text)
+
+
+#Кнопка для альт перевода
+alt_btn = ttk.Button(text='translate', command=get_alt_translation_of_text)
+alt_btn.pack(anchor=E, pady=70)
+
+#Вывод альтернативного перевода
+
+alt_trans_lbl = Label()
+alt_trans_lbl.pack(anchor=CENTER, pady=80)
 
 #Отображение окна
 root.mainloop()
